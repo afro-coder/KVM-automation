8c64bec64a2242cc5a63c32c9082fc9b6384426a
afrocoder <leon9923@gmail.com>
2021-07-15 21:18:10 +0530

    Added bash scripts

---
+++ bash-scripts/README.md
@@ -0,0 +1,3 @@
+# VM bash scripts
+
+These scripts specify various installation methods for libvirt. 
---
+++ bash-scripts/vm_with_disk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+virt-install --connect qemu:///system \
+        --name "${VM_NAME}" \
+        --disk "${VM_DISK_PATH},target.bus=virtio \
+        --boot hd \
+        --memory=1048 \
+        --noautoconsole \
+        --graphics none \
+        --os-variant "${VM_OS_VARIANT}" \
+        --vcpus 2 \
+        --cpu host \
+        --os-type linux \
+        --network network=br0,model=virtio \ # Change this
+        --console pty,target_type=serial \
+        #--extra-args "console=ttyS0,115200n8 serial" \
+        #--wait=-1
